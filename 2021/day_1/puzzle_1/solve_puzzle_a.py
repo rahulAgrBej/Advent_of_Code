1b4c1541eec23f9ea3aed12f6d44216d4259c249
rahulAgrBej <rahul.agrawal.b@gmail.com>
2021-12-01 13:53:53 -0500

    solution part A of the puzzle

---
+++ 2021/day_1/puzzle_1/solve_puzzle_a.py
@@ -0,0 +1,20 @@
+
+# read input
+f = open('input.txt')
+depths = f.readlines()
+f.close()
+
+# counter
+num_larger = 0
+
+for idx in range(len(depths)):
+
+    # stripping input of new lines and casting to int
+    prev_depth = int(depths[idx - 1].rstrip('\n'))
+    curr_depth = int(depths[idx].rstrip('\n'))
+
+    # updating counter
+    if curr_depth > prev_depth:
+        num_larger += 1
+
+print(f'Number of measurements that are larger than the previous measurement: {num_larger}')
